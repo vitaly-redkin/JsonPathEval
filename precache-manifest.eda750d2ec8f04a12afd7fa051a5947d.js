a2809edc031362372020fc115a344f8e09ac825f
vitaly-redkin <v21267@gmail.com>
2020-09-22 11:04:00 +0300

    Updates

---
+++ precache-manifest.eda750d2ec8f04a12afd7fa051a5947d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887a6c61e5ef3d65fd1b09e48960dfee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a40a6b6c37a5ca3e2754",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dda10c5943ba8cc8262d",
+    "url": "/static/css/main.ee5980db.chunk.css"
+  },
+  {
+    "revision": "a40a6b6c37a5ca3e2754",
+    "url": "/static/js/2.49f2350f.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.49f2350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda10c5943ba8cc8262d",
+    "url": "/static/js/main.a95ea771.chunk.js"
+  },
+  {
+    "revision": "0ae51f691074e8e57ad4",
+    "url": "/static/js/runtime-main.2cc9ce7e.js"
+  }
+]);
